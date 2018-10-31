65dfcd11dd8384bba9329d0fff661a34e1bf9cee
NickNaso <nicoladelgobbo@gmail.com>
2018-10-31 22:55:55 +0100

    Added option to force rebuild

--- experiments/2_import_c_export_go_to_node/build.sh
+++ experiments/2_import_c_export_go_to_node/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash          
 echo Starting build process ...
 echo Build Go library ...
-go build -x -o libmath.a -buildmode=c-archive libmath.go && \
+go build -a -x -o libmath.a -buildmode=c-archive libmath.go && \
 echo Build Node.js native add-on ... && \
 npm i && \
 echo Node.js native add-on successfully created. && \
