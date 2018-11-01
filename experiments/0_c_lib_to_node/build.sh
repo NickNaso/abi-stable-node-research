96cfcb9459c8fc495ecf3a3c7d2386ad75b322fd
NickNaso <nicoladelgobbo@gmail.com>
2018-11-01 17:10:11 +0100

    Complete example on importing  C static library

--- experiments/0_c_lib_to_node/build.sh
+++ experiments/0_c_lib_to_node/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash          
 echo Starting build process ...
-echo Build Go library ...
-go build -a -x -o libmath.a -buildmode=c-archive libmath.go && \
+echo Build C library ...
+gcc -c sum.cc && \
+ar -rc libsum.a sum.o
 echo Build Node.js native add-on ... && \
 npm i && \
 echo Node.js native add-on successfully created. && \
