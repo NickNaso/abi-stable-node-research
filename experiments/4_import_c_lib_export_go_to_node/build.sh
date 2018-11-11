093bb06df9d2984c221894f4acb8ccc77c8d4582
NickNaso <nicoladelgobbo@gmail.com>
2018-11-11 11:24:51 +0100

    Scaffolding for new experiments

---
+++ experiments/4_import_c_lib_export_go_to_node/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash          
+echo Starting build process ...  && \
+echo Build C library ...  && \
+#rm -rf main.o && \
+#rm -rf sum.o && \
+#rm -rf libsum.a && \
+#rm -rf main.o && \
+#rm -rf libmath.a && \
+gcc -c sum.c && \
+ar -rcs libsum.a sum.o && \
+ranlib libsum.a && \
+echo Build Go library ...  && \
+go build -a -x -o libmath.a -buildmode=c-archive libmath.go && \
+#ranlib libmath.a && \
+echo Compile C program && \
+gcc -c main.cc && \
+gcc -o main main.o -pthread -L. -lmath -lsum && \
+./main
+
