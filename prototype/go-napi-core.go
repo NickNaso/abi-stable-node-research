94276c4cfc34db374213072e01fce97f718279ac
NickNaso <nicoladelgobbo@gmail.com>
2019-02-13 00:26:31 +0100

    Base code for the addon written in Go

--- prototype/go-napi-core.go
+++ prototype/go-napi-core.go
@@ -1,5 +1,25 @@
 package main
 
+// #cgo CFLAGS: -I.
+// #cgo LDFLAGS: -L. -lnode_api
+// #include <node_api.h>
 import "C"
 
+//export Method
+func Method(env C.napi_env, info C.napi_callback_info) C.napi_value {
+	var res C.napi_value
+	C.napi_create_int32(env, 5, &res)
+	return res
+}
+
 func main() {}
+
+/*
+napi_value Method(napi_env env, napi_callback_info info) {
+  napi_status status;
+  napi_value res;
+  status = napi_create_int32(env, Add(1, 4), &res);
+  assert(status == napi_ok);
+  return res;
+}
+*/
