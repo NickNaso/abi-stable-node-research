92a33c6a6c8eb95e0d29e87f281339623c0309f1
NickNaso <nicoladelgobbo@gmail.com>
2019-02-14 23:13:28 +0100

    Removed unuseful part in the code

--- prototype/go-napi.go
+++ prototype/go-napi.go
@@ -13,13 +13,3 @@ func Method(env C.napi_env, info C.napi_callback_info) C.napi_value {
 }
 
 func main() {}
-
-/*
-napi_value Method(napi_env env, napi_callback_info info) {
-  napi_status status;
-  napi_value res;
-  status = napi_create_int32(env, Add(1, 4), &res);
-  assert(status == napi_ok);
-  return res;
-}
-*/
