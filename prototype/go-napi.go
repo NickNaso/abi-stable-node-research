8105f40cc8fe1f80dcb29a9bbf9d680e0795d0a9
NickNaso <nicoladelgobbo@gmail.com>
2019-02-23 17:30:23 +0100

    Prototype of buiding system is ok

--- prototype/go-napi.go
+++ prototype/go-napi.go
@@ -1,5 +1,6 @@
 package main
 
+// #include <stdlib.h>
 // #cgo CFLAGS: -I.
 // #cgo LDFLAGS: -L. -lnode_api
 // #include <node_api.h>
@@ -8,7 +9,7 @@ import "C"
 //export Method
 func Method(env C.napi_env, info C.napi_callback_info) C.napi_value {
 	var res C.napi_value
-	C.napi_create_int32(env, 5, &res)
+	C.napi_create_int32(env, C.int(5), &res)
 	return res
 }
 
