468066aa113ce30d12c2d347132923dd383da504
NickNaso <nicoladelgobbo@gmail.com>
2019-04-12 10:32:06 +0200

    Move the addon in a separate folder

--- indirection-system/build.sh
+++ indirection-system/build.sh
@@ -2,8 +2,8 @@
 echo Cleanup previous build ...  && \
 rm -rf libnode_api.a && \
 rm -rf node_api.o && \
-rm -rf libgoaddon.a && \
-rm -rf libgoaddon.h && \
+rm -rf ./addon/libgoaddon.a && \
+rm -rf ./addon/libgoaddon.h && \
 echo Start building N-API stub ...  && \
 echo Build C library ...  && \
 gcc -c node_api.c && \
@@ -12,7 +12,10 @@ ranlib libnode_api.a && \
 echo N-API stub successfully builded. && \
 echo Build Go library ...  && \
 go build -a -x -o libgoaddon.a -buildmode=c-archive go-napi.go && \
+mv ./libgoaddon.a ./addon/libgoaddon.a && \
+mv ./libgoaddon.h ./addon/libgoaddon.h && \
 echo Build Node.js native add-on ... && \
+cd addon && \
 npm i && \
 echo Node.js native add-on successfully created. && \
 echo Starting test ... && \
--- indirection-system/go-napi.go
+++ indirection-system/go-napi.go
@@ -4,14 +4,6 @@ package main
 // #cgo CFLAGS: -I.
 // #cgo LDFLAGS: -L. -lnode_api
 // #include <node_api.h>
-/*
-
-static napi_value ooooooooo(napi_env env, napi_callback_info info) {
-  napi_value res;
-  napi_create_int32(env, 5, &res);
-  return res;
-}
-*/
 import "C"
 
 //export Method
@@ -22,10 +14,11 @@ func Method(env C.napi_env, info C.napi_callback_info) C.napi_value {
 }
 
 //export Initialize
-func Initialize(env C.napi_env, exportss C.napi_value) C.napi_value {
-	// napi_property_descriptor desc = { "createInt32", 0, Method, 0, 0, 0, napi_default, 0 };
-	// status = napi_define_properties(env, exports, 1, &desc);
-	// return exports;
+func Initialize(env C.napi_env, exports C.napi_value) C.napi_value {
+	name := C.CString("createInt32")
+	desc := C.napi_property_descriptor{name, nil, (C.napi_callback)(nil), nil, nil, nil, C.napi_default, nil}
+	C.napi_define_properties(env, exports, 1, &desc)
+	return exports
 }
 
 func main() {}
