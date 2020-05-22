a83502b58363fa7cca11f49ab0b117f96a4063bc
NickNaso <nicoladelgobbo@gmail.com>
2020-05-22 19:49:05 +0200

    Some fixes for example 0.

--- experiments/0_c_lib_to_node/build.sh
+++ experiments/0_c_lib_to_node/build.sh
@@ -1,11 +1,15 @@
-#!/bin/bash          
+#!/bin/bash
+echo Cleanup ... && \
+rm -rf libsum.a && \
+rm -rf sum.o && \
+echo Cleanup successfully executed. && \
 echo Starting build process ...
 echo Build C library ...
-gcc -c sum.c -mmacosx-version-min=10.7 && \
+gcc -c sum.c && \
 ar -rc libsum.a sum.o
 echo Build Node.js native add-on ... && \
 npm i && \
 echo Node.js native add-on successfully created. && \
 echo Starting test ... && \
-node add.js && \
+npm test && \
 echo Test successfully ended.
--- experiments/0_c_lib_to_node/sum.h
+++ experiments/0_c_lib_to_node/sum.h
@@ -1,10 +1,10 @@
+#ifndef _SUM_H_
+#define _SUM_H_
 #ifdef __cplusplus 
 extern "C" {
 #endif
-    #ifndef _SUM_H
-    #define _SUM_H
-    double sum(double a, double b);
-    #endif
+    double sum(double a, double b);   
 #ifdef __cplusplus 
 }
 #endif
+#endif // _SUM_H_
